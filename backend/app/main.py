edee7a8e2598f5c890153df1b4668d12ba683335
Satish Dewasi <satishdewasi101@gmail.com>
2026-09-12 01:27:02 +0530

    added url for cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://neostats-frontend.onrender.com/"],
+    allow_origins=["http://localhost:5173", "https://neostats-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
